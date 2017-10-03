862ea3429e6f6fa4600f2ac909d18591563f3aba
Nightzxfx <paulo_valadares@msn.com>
2017-10-03 12:32:11 -0400

    Update Constructor example.java

--- Constructor example.java
+++ Constructor example.java
@@ -1,33 +1,55 @@
 package week5;
+import java.util.ArrayList;
 
 public class Player {
 		
 	private int Health;
-
-	
+	private String name;
+	//private String vocation;
 	
 	
 	public void setHealth(int h) {
 		Health = h;
 	}
 	
+	public void setName(String n) {
+		name = n;
+		
+	}
+	
 	public int getHealth() {
 		return Health;
 	}
 	
+	public String getName() {
+		return name;
+	}
 	
 	public static void main(String[] args) {
+		
+	ArrayList<String> vocation= new ArrayList<String>();
+	vocation.add("Paladin");
+	vocation.add("Knight");
+	vocation.add("Druid");
+	vocation.add("Sorcare");
+	
 		
 		Player Knight = new Player();
 		
 		Knight.setHealth(1000);
 		
-		System.out.println(Knight.getHealth());
+		Knight.setName("Paulo");
+		
+		System.out.println("Palyer: " + Knight.getName() + " Life " + Knight.getHealth() +" Vocation: " + vocation.get(1));
+		
+		
 		
 		Player Pally = new Player();
 		
 		Pally.setHealth(100);
+		Pally.setName("Valadares");
 		
-		System.out.println(Pally.getHealth());
+		System.out.println("Player: " + Pally.getName() + " Life: " + Pally.getHealth() + " Vocation: " + vocation.get(0));
 	}
 }
+
