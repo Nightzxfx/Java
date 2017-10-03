b5380b7258d7e4b7658dee0b1dfb4e09a777315a
Nightzxfx <paulo_valadares@msn.com>
2017-10-03 12:54:30 -0400

    Create whiledo.java

---
+++ whiledo.java
@@ -0,0 +1,20 @@
+Public class while {
+
+public static void main(String[] args) {
+
+int age = 10;
+
+while (age < 15) {
+sysout("Value of age: " + age);
+++ age;
+}
+}
+
+// using do
+
+do {
+sysout("Value of age: " + age);
+++ age;
+ while (age < 15)
+ }
+ }
