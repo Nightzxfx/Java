56b77702ae243247eba706f4565f24a6b7ee27a3
Nightzxfx <paulo_valadares@msn.com>
2017-10-03 11:55:38 -0400

    Update constructor.java

--- constructor.java
+++ constructor.java
@@ -23,20 +23,54 @@ public class product {
 		
 	}
 
+//function called scan, will return the price value and it's parameter is String Code ('java"), but you dont need it
+public double scan(String code) {
+	System.out.println(price);
+	return price;
+}	
+//function called void, nothing is returning, so if you call it will print the description
+public void printDescription() {
+	
+	System.out.println(description);
+	
+}
+//function called String and when called it will return the value in code "java"
+public String assignCode() {
+	
+	return code;
+	
+}
 	public static void main(String[] args) {
 		
-// when creating a new object is leave with not parametes it will takes from the first consctructor
-		product bla = new product();
-		
-//System.out.println(bla.code); will print "No code set"		
-		System.out.println(bla.code);
+
+// creatng a new object called test
+		product test = new product(10D, "java", "You should not buy it");
 		
 		
+// calling the function scan		
+		test.scan("java");
+
+//assgn
+		Double a = test.scan("java");
+		
+		System.out.println(a);
+		
+		test.printDescription();
+		
+		String newcode = test.assignCode();
+		
+		System.out.println(newcode);
 	}
+}
+
+//when creating a new object is leave with not parameters it will takes from the first constructor
+		//product test = new product();
+//System.out.println(test.code); will print "No code set"
+
 
 //	Is you give parameters or valuers you will use from there
-	//product bla = new product(10, "bkakva", "vwwe");
+	//product test = new product(10D, "java", "You should not buy it");
 	//                         price,  code, description
-	//System.out.println(bla.code); will print "bkakva"
-}
+	//System.out.println(bla.code); will print "java"
+
 
