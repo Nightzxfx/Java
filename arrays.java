887c08f2607a1c1d3bc5f7d11668dc5a29337b7d
Nightzxfx <paulo_valadares@msn.com>
2017-10-03 12:43:12 -0400

    Create arrays.java

---
+++ arrays.java
@@ -0,0 +1,20 @@
+// first import the array from java library
+
+import util.java.*;   // it will import all the library
+import util.java.ArrayList; // it will import only the ArrayList
+
+Public class Array {
+
+public static void main(String[] args) {
+		
+	ArrayList<String> vocation= new ArrayList<String>();  //t
+	vocation.add("Paladin");
+	vocation.add("Knight");
+	vocation.add("Druid");
+	vocation.add("Sorcare");
+  
+  }
+  }
+  
+  
+  Int Array[] = new Int[10];
